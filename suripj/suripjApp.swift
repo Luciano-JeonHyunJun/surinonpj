3c7a277ccaa715d6dd6b57c8c6fc206b30dae003
Luciano-JeonHyunJun <hyunjun050915@gmail.com>
2024-12-27 15:15:35 +0900

    Initial Commit

---
+++ suripj/suripjApp.swift
@@ -0,0 +1,17 @@
+//
+//  suripjApp.swift
+//  suripj
+//
+//  Created by Jeon HyunJun on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct suripjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ suripjTests/suripjTests.swift
@@ -0,0 +1,17 @@
+//
+//  suripjTests.swift
+//  suripjTests
+//
+//  Created by Jeon HyunJun on 12/27/24.
+//
+
+import Testing
+@testable import suripj
+
+struct suripjTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
