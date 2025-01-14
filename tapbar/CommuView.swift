c8cda8feea93cac73c554f20a357ae24d6639e3f
Luciano-JeonHyunJun <hyunjun050915@gmail.com>
2025-01-14 13:31:26 +0900

    fix uni logo is

--- tapbar/CommuView.swift
+++ tapbar/CommuView.swift
@@ -1,12 +1,64 @@
 import SwiftUI
 
 struct CommuView: View {
+    // 대학 목록 (6개 채널)
+    let universities = [
+        "고려대 논술", "연세대 논술", "서울대 논술", "한양대 논술", "이화여대 논술", "중앙대 논술"
+    ]
+    
+    // 선택된 대학
+    @State private var selectedUniversity: String?
+
     var body: some View {
-        Text("CommuView 화면")
-            .font(.title)
-            .foregroundColor(.green)
+        NavigationView {
+            VStack {
+                // 채널 형식의 스와이프 가능한 리스트
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(universities, id: \.self) { university in
+                            VStack {
+                                ZStack {
+                                    // 채널 카드
+                                    RoundedRectangle(cornerRadius: 15)
+                                        .fill(Color.blue)
+                                        .frame(width: 200, height: 120)
+                                        .shadow(radius: 5)
+                                    
+                                    Text(university)
+                                        .font(.title2)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.white)
+                                        .padding()
+                                }
+                                .onTapGesture {
+                                    // 대학을 선택했을 때 해당 대학 이름을 selectedUniversity에 저장
+                                    selectedUniversity = university
+                                }
+                                
+                                // 선택된 대학에 대한 정보 표시
+                                if selectedUniversity == university {
+                                    Text("\(university)에 대한 세부 정보입니다.")
+                                        .font(.body)
+                                        .padding()
+                                        .foregroundColor(.black)
+                                }
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                .frame(height: 180)
+                
+                Spacer()
+            }
+            .navigationTitle("대학 논술 채널")
+        }
     }
 }
 
-
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
 
--- tapbar/unizip.swift
+++ tapbar/unizip.swift
@@ -27,25 +27,139 @@ struct UniZipView: View {
                 ScrollView {
                     VStack(spacing: 10) {
                         if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true) {
-                            universityRow("연세대학교", logoName: "yonsei_logo", destination: YonseiUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                            universityRow("아주대학교", logoName: "!ajou_logo", destination: AjouUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true,hasMedicalEssay: true)
                         }
-                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true) {
-                            universityRow("고려대학교", logoName: "korea_logo", destination: KoreaUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true){
+                            universityRow("중앙대학교", logoName: "!chungang_logo", destination: ChungangUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
                         }
-                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false) {
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true){
+                            universityRow("이화여자대학교", logoName: "!ewhawoen's_logo", destination: EwhaFMUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true){
+                            universityRow("인하대학교", logoName: "!inha_logo", destination: InhaUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true){
+                            universityRow("고려대학교 세종캠퍼스", logoName: "!koreaC_logo", destination: KoreaSJUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true){
+                            universityRow("경희대학교", logoName:"!kyunghee_logo", destination: KyungheeUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true){
+                            universityRow("경북대학교", logoName: "!kyungpook_logo", destination: KyungpookUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true){
+                            universityRow("부산대학교", logoName: "!pusan_logo", destination: PusanUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true){
+                            universityRow("숙명여자대학교", logoName: "!sookmyungwomen's_logo", destination: SookmyungFMUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true){
+                            universityRow("성균관대학교", logoName: "!sungkyunkwan_logo", destination: SungkyunkwanUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true){
+                            universityRow("연세대학교 미래캠퍼스", logoName: "!yonseiC_logo", destination: YonseiMRUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("한국항공대학교", logoName: "aerospace_logo", destination: AerospaceUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("가톨릭대학교", logoName: "catholic_logo", destination: CatholicUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("단국대학교", logoName: "dankook_logo", destination: DankookUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("단국대학교 죽전캠퍼스", logoName: "dankookC_logo", destination: DankookCSUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("동덕 여자 대학교", logoName: "dongduk_logo", destination: DonggukUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("동국대학교", logoName: "dongguk_logo", destination: DonggukUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("덕성 여자 대학교", logoName: "duksung_logo", destination: DuksungFMUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("을지대학교", logoName: "eulji_logo", destination: EuljiUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("한국 외국어 대학교", logoName: "foreign_logo", destination: ForeignUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("가천대학교", logoName: "gachon_logo", destination: GachonUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("한신대학교", logoName: "hanshin_logo", destination: HanshinUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
                             universityRow("한양대학교", logoName: "hanyang_logo", destination: HanyangUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
                         }
-                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true) {
-                            universityRow("가톨릭대학교", logoName: "catholic_logo", destination: CatholicUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: true)
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("홍익대학교", logoName: "hongik_logo", destination: HongikUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
                         }
-                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false) {
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("홍익대학교 세종캠퍼스", logoName: "hongikC_logo", destination: HongikSJUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("강남대학교", logoName: "kangnam_logo", destination: KangnamUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
                             universityRow("건국대학교", logoName: "konkuk_logo", destination: KonkukUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("국민대학교", logoName: "kookmin_logo", destination: KookminUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("고려대학교", logoName: "korea_logo", destination: KoreaUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("광운대학교", logoName: "kwangwoon_logo", destination: KwangwoonUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
                         }
                         if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
                             universityRow("경기대학교", logoName: "kyonggi_logo", destination: KyonggiUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
                         }
                         if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
-                            universityRow("광운대학교", logoName: "kwangwoon_logo", destination: KwangwoonUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                            universityRow("삼육대학교", logoName: "sahmyook _logo", destination: SahmyookUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("삼명대학교", logoName: "sangmyung _logo", destination: SangmyungUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("세종대학교", logoName: "sejong_logo", destination: SejongUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("서경대학교", logoName: "seokyeong_logo", destination: SeokyeongUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("서울 여자 대학교", logoName: "seoulFM_logo", destination: SeoulFMUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("신한대학교", logoName: "shinhan_logo", destination: ShinhanUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("서울 시립대학교", logoName: "sirip_logo", destination: SiripUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("서강대학교", logoName: "sogang_logo", destination: SogangUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("숭실대학교", logoName: "soongsil_logo", destination: SoongsillUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("서울 과학 기술 대학교", logoName: "sst_logo", destination: SstUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("성신 여자 대학교", logoName: "sungshinFM_logo", destination: SungshinFMUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("수원대학교", logoName: "suwon_logo", destination: SuwonUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("한국기술교육대학교", logoName: "techteach_logo", destination: TeckteachUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
+                        }
+                        if shouldShowUniversity(hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false){
+                            universityRow("연세대학교", logoName: "yonsei_logo", destination: YonseiUniversityView(), hasMathEssay: true, hasHumanitiesEssay: true, hasMedicalEssay: false)
                         }
                     }
                     .padding()
@@ -182,5 +296,7 @@ struct UniZipView_Previews: PreviewProvider {
         UniZipView()
     }
 }
+
+
 
 
